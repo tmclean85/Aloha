881b8c444acfd503a914a711263e456a4f9f67ab
tmclean85 <trevor.a.mclean@gmail.com>
2017-04-22 23:38:23 -0700

    Implemented jQuery smoothscroll for navbar links

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,13 +1,21 @@
-/*$(function() {
 
-  
-//smooth scrolling
 //form submit
 
-});*/
-
 $('.item-carousel').flickity({
-  // options
   cellAlign: 'left',
   contain: true
+});
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
 });
