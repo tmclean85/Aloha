8962d21843055dd8c070170387d365fe926039f2
tmclean85 <trevor.a.mclean@gmail.com>
2017-04-22 22:36:27 -0700

    Implemented flickity jQuery, fixed fonts, edited for semantics

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+/*$(function() {
+
+  
+//smooth scrolling
+//form submit
+
+});*/
+
+$('.item-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
+});
