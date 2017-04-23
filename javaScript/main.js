39782550b42e3b1312dd06344024779d05bbcac0
tmclean85 <trevor.a.mclean@gmail.com>
2017-04-23 14:18:15 -0700

    Completed styling on input field/button, alerts need work

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,11 +1,10 @@
 
-//form submit
-
+//FLICKITY
 $('.item-carousel').flickity({
   cellAlign: 'left',
   contain: true
 });
-
+//SMOOTHSCROLL
 $(document).ready(function(){
   $("a").on('click', function(event) {
     if (this.hash !== "") {
@@ -19,3 +18,14 @@ $(document).ready(function(){
     } 
   });
 });
+//FORMSUBMIT
+$("form").submit(function(){
+  if (email=='')
+    alert("Please submit a valid e-mail address");
+    else if (email!=='') 
+    {
+      
+      alert("Thanks for subscribing!");
+    }
+}
+);
